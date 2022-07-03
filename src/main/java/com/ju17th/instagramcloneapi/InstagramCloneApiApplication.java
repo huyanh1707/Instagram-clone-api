ee760fe6016759e9d791bb83b17bc7b9394b8239
Huy Anh <ndhuyanh1707@gmail.com>
2022-07-03 10:33:36 +0700

    spring initialize

---
+++ src/main/java/com/ju17th/instagramcloneapi/InstagramCloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.ju17th.instagramcloneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramCloneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ju17th/instagramcloneapi/InstagramCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ju17th.instagramcloneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
