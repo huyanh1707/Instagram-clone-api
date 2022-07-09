3387ad55577e4b9833aaa20ae5919a47b94d8183
Huy Anh <ndhuyanh1707@gmail.com>
2022-07-09 13:39:50 +0700

    Login signup api

--- src/main/java/com/ju17th/instagramcloneapi/InstagramCloneApiApplication.java
+++ src/main/java/com/ju17th/instagramcloneapi/InstagramCloneApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InstagramCloneApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(InstagramCloneApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramCloneApiApplication.class, args);
+    }
 }
--- src/test/java/com/ju17th/instagramcloneapi/InstagramCloneApiApplicationTests.java
+++ src/test/java/com/ju17th/instagramcloneapi/InstagramCloneApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class InstagramCloneApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
